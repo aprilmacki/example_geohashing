63ea91af8805fb810a1935a56ad8b02476b76dcc
April <aprilmack@posteo.net>
2025-02-23 18:03:49 -0600

    Add various calculations
    Add displacement calculation and unit tests
    Started "cells with radius" calculation and some unit tests

--- geohash.py
+++ geohash.py
@@ -1,6 +1,23 @@
+import math
+from copy import copy
 from dataclasses import dataclass
 
-def to_geohash(lat: float, lon: float, precision: int):
+@dataclass
+class CellBoundary:
+    start_lat: float    # North side
+    end_lat: float      # South side
+    start_lon: float    # West side
+    end_lon: float      # East side
+
+@dataclass
+class Coordinates:
+    lat: float
+    lon: float
+
+
+EARTH_RADIUS_KM = 6371
+
+def calc_geohash(point: Coordinates, precision: int):
     lat_min = -90
     lat_max = 90
     lon_min = -180
@@ -13,7 +30,7 @@ def to_geohash(lat: float, lon: float, precision: int):
         if is_lon_bit:
             # Divide left-right
             lon_mid = (lon_min + lon_max) / 2
-            if lon >= lon_mid:
+            if point.lon >= lon_mid:
                 new_bit = 1
                 lon_min = lon_mid
             else:
@@ -22,7 +39,7 @@ def to_geohash(lat: float, lon: float, precision: int):
         else:
             # Divide up-down
             lat_mid = (lat_min + lat_max) / 2
-            if lat >= lat_mid:
+            if point.lat >= lat_mid:
                 new_bit = 0
                 lat_min = lat_mid
             else:
@@ -31,14 +48,7 @@ def to_geohash(lat: float, lon: float, precision: int):
         geohash = (geohash << 1) | new_bit
     return geohash
 
-@dataclass
-class GeoBoundary:
-    start_lat: float
-    end_lat: float
-    start_lon: float
-    end_lon: float
-
-def to_lat_lon_boundary(geohash: int, precision: int) -> GeoBoundary:
+def calc_cell_boundary(cell_geohash: int, precision: int) -> CellBoundary:
     lat_min = -90
     lat_max = 90
     lon_min = -180
@@ -46,7 +56,7 @@ def to_lat_lon_boundary(geohash: int, precision: int) -> GeoBoundary:
 
     is_lon_bit = True
     for i in range(precision-1, -1, -1):
-        bit = geohash & (1 << i)
+        bit = cell_geohash & (1 << i)
         if is_lon_bit:
             lon_mid = (lon_min + lon_max) / 2
             if bit:
@@ -65,22 +75,118 @@ def to_lat_lon_boundary(geohash: int, precision: int) -> GeoBoundary:
                 lat_min = lat_mid
         is_lon_bit = not is_lon_bit
 
-    return GeoBoundary(
+    return CellBoundary(
         start_lat=lat_min,
         end_lat=lat_max,
         start_lon=lon_min,
         end_lon=lon_max,
     )
 
-def calc_neighbors(lat: float, lon: float, radius: float):
-    # TODO: Compute north point, west point, south point, and east point.
 
-    # TODO: If west point not included in boundary, add west geohashes until it is included
-    # TODO: If east point not included in boundary, add east geohashes until it is included
-    # TODO: If north point not included in boundary, add geohashes north of current geohash set until it's included
-    # TODO: If south point not included in boundary, add geohashes south of current geohash set until it's included
+def is_west_of(origin_point_lon: float, boundary_point_lon: float, test_point_lon) -> bool:
+    distance_west_of = lambda origin_lon, point_lon : origin_lon - point_lon + 360 if origin_lon < 0 < point_lon else origin_lon - point_lon
+    boundary_dist = distance_west_of(origin_point_lon, boundary_point_lon)
+    test_point_dist = distance_west_of(origin_point_lon, test_point_lon)
+    # TODO: unit test this
+    return test_point_dist > boundary_dist
 
+def is_east_of(origin_point_lon: float, boundary_point_lon: float, test_point_lon: float) -> bool:
+    distance_east_of = lambda origin_lon, point_lon : point_lon - origin_lon + 360 if origin_lon > 0 > point_lon else point_lon - origin_lon
+    boundary_dist = distance_east_of(origin_point_lon, boundary_point_lon)
+    test_point_dist = distance_east_of(origin_point_lon, test_point_lon)
+    # TODO: unit test this
+    return test_point_dist > boundary_dist
+
+def calc_cells_within_radius(point: Coordinates, precision: int, radius: float) -> list[int]:
+    '''
+    Calculate the geohashes of the cells that contain points within radius km of `coords`.
+
+    This calculates a rectangular boundary around `coords` with dimensions 2*radius by 2*radius.
+    Any cell that overlaps this rectanglar boundary is returned.
+    A rectangular boundary is used for simplicity, rather than a circular one.
+
+    '''
+    ghash = calc_geohash(point, precision)
+    ghashes = [ghash]
+    ew_hashes = [ghash]
+    boundary: CellBoundary = calc_cell_boundary(ghash, precision)
+
+    west_point = displace_point(point, radius, 1.5 * math.pi)
+    current_cell_hash = ghash
+    while is_west_of(point.lon, boundary.start_lon, west_point.lon):
+        neighbor_cell_hash = calc_neighbor_cell(current_cell_hash, precision, 0, -1)
+        neighbor_cell_boundary = calc_cell_boundary(neighbor_cell_hash, precision)
+        boundary.start_lon = neighbor_cell_boundary.start_lon
+        ghashes += neighbor_cell_hash
+        ew_hashes += neighbor_cell_hash
+        current_cell_hash = neighbor_cell_hash
+
+    east_point = displace_point(point, radius, math.pi / 2)
+    current_cell_hash = ghash
+    while east_point.lon > boundary.end_lon:
+        # TODO: Handle wrap around
+        neighbor_cell_hash = calc_neighbor_cell(current_cell_hash, precision, 0, 1)
+        neighbor_cell_boundary = calc_cell_boundary(neighbor_cell_hash, precision)
+        boundary.end_lon = neighbor_cell_boundary.end_lon
+        ghashes += neighbor_cell_hash
+        ew_hashes += neighbor_cell_hash
+        current_cell_hash = neighbor_cell_hash
+
+    north_point = displace_point(point, radius, 0)
+    current_ew_hashes = copy(ew_hashes)
+    while north_point.lat > boundary.start_lat:
+        # TODO: Handle wrap around
+
+        new_ew_hashes = []
+        for ew_hash in current_ew_hashes:
+            neighbor_cell_hash = calc_neighbor_cell(ew_hash, precision, 1, 0)
+            ghashes += neighbor_cell_hash
+            new_ew_hashes += neighbor_cell_hash
+
+        neighbor_cell_boundary = calc_cell_boundary(new_ew_hashes[0], precision)
+        boundary.start_lat = neighbor_cell_boundary.start_lat
+        current_ew_hashes = new_ew_hashes
+
+    south_point = displace_point(point, radius, math.pi)
+    current_ew_hashes = copy(ew_hashes)
+    while south_point.lat < boundary.end_lat:
+        # TODO: Handle wrap around
+        new_ew_hashes = []
+        for ew_hash in current_ew_hashes:
+            neighbor_cell_hash = calc_neighbor_cell(ew_hash, precision, -1, 0)
+            ghashes += neighbor_cell_hash
+            new_ew_hashes += neighbor_cell_hash
+
+        neighbor_cell_boundary = calc_cell_boundary(new_ew_hashes[0], precision)
+        boundary.end_lat = neighbor_cell_boundary.end_lat
+        current_ew_hashes = new_ew_hashes
+
+    return ghashes
+
+def calc_neighbor_cell(geohash: int, precision: int, row_offset: int, col_offset: int) -> int:
+    # Positive row offset is north-ward
+    # Positive column offset is east-ward
     pass
 
+def displace_point(start_point: Coordinates, offset_km: float, angle_from_n_rad: float) -> Coordinates:
+    # See http://www.movable-type.co.uk/scripts/latlong.html
+    angular_distance = offset_km / EARTH_RADIUS_KM
+    dest_lat_rad = math.asin(math.sin(math.radians(start_point.lat)) * math.cos(angular_distance) + math.cos(math.radians(start_point.lat)) * math.sin(angular_distance) * math.cos(angle_from_n_rad))
 
+    dest_lon_rad = math.radians(start_point.lon) + math.atan2(
+        math.sin(angle_from_n_rad) * math.sin(angular_distance) * math.cos(math.radians(start_point.lat)),
+        math.cos(angular_distance) - math.sin(math.radians(start_point.lat)) * math.sin(dest_lat_rad)
+    )
 
+    dest_lat = math.degrees(dest_lat_rad)
+    dest_lon = math.degrees(dest_lon_rad)
+
+    return Coordinates(lat=dest_lat, lon=dest_lon)
+
+def calc_distance_km(point1: Coordinates, point2: Coordinates) -> float:
+    # See http://www.movable-type.co.uk/scripts/latlong.html
+    delta_lat = math.radians(point2.lat - point1.lat)
+    delta_lon = math.radians(point2.lon - point1.lon)
+
+    a = math.pow(math.sin(delta_lat / 2), 2) + math.cos(math.radians(point1.lat)) * math.cos(math.radians(point2.lat)) * math.pow(math.sin(delta_lon / 2), 2)
+    return 2 * math.atan2(math.sqrt(a), math.sqrt(1 - a)) * EARTH_RADIUS_KM
--- tests/geohash_test.py
+++ tests/geohash_test.py
@@ -1,52 +1,59 @@
+import math
 import random
 import unittest
 
 import geohash
+from geohash import Coordinates, CellBoundary
+
 
 def print_point(lat: float, lon: float):
     print(f'{lat},{lon},#f00,circle')
 
-def print_boundary(boundary: geohash.GeoBoundary):
+def print_boundary(boundary: geohash.CellBoundary):
     print(f'{boundary.start_lat},{boundary.start_lon},#333,circle')
     print(f'{boundary.start_lat},{boundary.end_lon},#333,circle')
     print(f'{boundary.end_lat},{boundary.start_lon},#333,circle')
     print(f'{boundary.end_lat},{boundary.end_lon},#333,circle')
 
 class GeoHashTest(unittest.TestCase):
-    def test_to_geohash_odd(self):
-        result = geohash.to_geohash(41.881832, -87.623177, 25)
+    def test_calc_geohash(self):
+        # Odd precision
+        result = geohash.calc_geohash(Coordinates(41.881832, -87.623177), 25)
         self.assertEqual(6300988, result)
 
-    def test_to_geohash_even(self):
-        result = geohash.to_geohash(41.881832, -87.623177, 20)
+        # Even precision
+        result = geohash.calc_geohash(Coordinates(41.881832, -87.623177), 20)
         self.assertEqual(196905, result)
 
-    def test_to_boundary_odd(self):
-        result: geohash.GeoBoundary = geohash.to_lat_lon_boundary(6300988, 25)
-
+    def test_calc_cell_boundary(self):
+        # Odd precision
+        result: geohash.CellBoundary = geohash.calc_cell_boundary(6300988, 25)
         print_boundary(result)
-
         self.assertEqual(41.8798828125, result.start_lat)
         self.assertEqual(41.923828125, result.end_lat)
         self.assertEqual(-87.626953125, result.start_lon)
         self.assertEqual(-87.5830078125, result.end_lon)
 
-    def test_to_boundary_even(self):
-        result: geohash.GeoBoundary = geohash.to_lat_lon_boundary(196905, 20)
-
+        # Even precision
+        result: geohash.CellBoundary = geohash.calc_cell_boundary(196905, 20)
         print_boundary(result)
-
         self.assertEqual(41.8359375, result.start_lat)
         self.assertEqual(42.01171875, result.end_lat)
         self.assertEqual(-87.890625, result.start_lon)
         self.assertEqual(-87.5390625, result.end_lon)
 
-    def test_random_conversion_odd(self):
+    def test_random_point_to_geohash_to_boundary_even(self):
+        self.t_random_point_to_geohash_to_boundary(20)
+
+    def test_random_point_to_geohash_to_boundary_odd(self):
+        self.t_random_point_to_geohash_to_boundary(25)
+
+    def t_random_point_to_geohash_to_boundary(self, precision: int):
         random_lat = random.uniform(-90, 90)
         random_lon = random.uniform(-180, 180)
 
-        ghash = geohash.to_geohash(random_lat, random_lon, 25)
-        boundary: geohash.GeoBoundary = geohash.to_lat_lon_boundary(ghash, 25)
+        ghash = geohash.calc_geohash(Coordinates(lat=random_lat, lon=random_lon), precision)
+        boundary: geohash.CellBoundary = geohash.calc_cell_boundary(ghash, precision)
 
         self.assertGreaterEqual(random_lat, boundary.start_lat)
         self.assertLessEqual(random_lat, boundary.end_lat)
@@ -56,19 +63,79 @@ class GeoHashTest(unittest.TestCase):
         print_point(random_lat, random_lon)
         print_boundary(boundary)
 
-    def test_random_conversion_even(self):
-        random_lat = random.uniform(-90, 90)
-        random_lon = random.uniform(-180, 180)
+    def test_displace_point_north(self):
+        start_coords = Coordinates(lat=41.881832, lon=-87.623177)
+        end_coords: Coordinates = geohash.displace_point(start_coords, 1.0, 0)
 
-        ghash = geohash.to_geohash(random_lat, random_lon, 20)
-        boundary: geohash.GeoBoundary = geohash.to_lat_lon_boundary(ghash, 20)
+        self.assertAlmostEqual(41.890825216059184, end_coords.lat, 5)
+        self.assertEqual(-87.623177 , end_coords.lon)
 
-        self.assertGreaterEqual(random_lat, boundary.start_lat)
-        self.assertLessEqual(random_lat, boundary.end_lat)
-        self.assertGreaterEqual(random_lon, boundary.start_lon)
-        self.assertLessEqual(random_lon, boundary.end_lon)
+        actual_distance = geohash.calc_distance_km(start_coords, end_coords)
+        self.assertAlmostEqual(1.0, actual_distance, 5)
 
-        print_point(random_lat, random_lon)
-        print_boundary(boundary)
+    def test_displace_point_south(self):
+        start_coords = Coordinates(lat=41.881832, lon=-87.623177)
+        end_coords: Coordinates = geohash.displace_point(start_coords, 1.0, math.pi)
 
+        self.assertAlmostEqual(41.87283878394081, end_coords.lat, 5)
+        self.assertEqual(-87.623177 , end_coords.lon)
+
+        actual_distance = geohash.calc_distance_km(start_coords, end_coords)
+        self.assertAlmostEqual(1.0, actual_distance, 5)
+
+    def test_displace_point_east(self):
+        start_coords = Coordinates(lat=41.881832, lon=-87.623177)
+        end_coords: Coordinates = geohash.displace_point(start_coords, 1.0, math.pi / 2)
+
+        self.assertAlmostEqual(41.881832, end_coords.lat, 5)
+        self.assertAlmostEqual(-87.6110978396452, end_coords.lon, 5)
+
+        actual_distance = geohash.calc_distance_km(start_coords, end_coords)
+        self.assertAlmostEqual(1.0, actual_distance, 5)
+
+    def test_displace_point_west(self):
+        start_coords = Coordinates(lat=41.881832, lon=-87.623177)
+        end_coords: Coordinates = geohash.displace_point(start_coords, 1.0, 3 * math.pi / 2)
+
+        self.assertAlmostEqual(41.881832, end_coords.lat, 5)
+        self.assertAlmostEqual(-87.6352561603548, end_coords.lon, 5)
+
+        actual_distance = geohash.calc_distance_km(start_coords, end_coords)
+        self.assertAlmostEqual(1.0, actual_distance, 5)
+
+    def test_is_west_of(self):
+        self.assertTrue(geohash.is_west_of(origin_point_lon=104, boundary_point_lon=100, test_point_lon=90))
+
+    def test_is_west_of_negative(self):
+        self.assertTrue(geohash.is_west_of(origin_point_lon=-104, boundary_point_lon=-110, test_point_lon=-113))
+
+    def test_is_west_of_test_point_crosses_180(self):
+        self.assertTrue(geohash.is_west_of(origin_point_lon=-168, boundary_point_lon=-170, test_point_lon=178))
+
+    def test_is_west_of_test_point_crosses_0(self):
+        self.assertTrue(geohash.is_west_of(origin_point_lon=12, boundary_point_lon=10, test_point_lon=-3))
+
+    def test_is_west_of_boundary_point_crosses_180(self):
+        self.assertFalse(geohash.is_west_of(origin_point_lon=-173, boundary_point_lon=178, test_point_lon=-179))
+
+    def test_is_west_of_boundary_point_crosses_0(self):
+        self.assertFalse(geohash.is_west_of(origin_point_lon=7, boundary_point_lon=-2, test_point_lon=1))
+
+    def test_is_east_of(self):
+        self.assertTrue(geohash.is_east_of(origin_point_lon=104, boundary_point_lon=110, test_point_lon=114))
+
+    def test_is_east_of_negative(self):
+        self.assertTrue(geohash.is_east_of(origin_point_lon=-104, boundary_point_lon=-100, test_point_lon=-95))
+
+    def test_is_east_of_test_point_crosses_180(self):
+        self.assertTrue(geohash.is_east_of(origin_point_lon=168, boundary_point_lon=170, test_point_lon=-178))
+
+    def test_is_east_of_test_point_crosses_0(self):
+        self.assertTrue(geohash.is_east_of(origin_point_lon=-12, boundary_point_lon=-10, test_point_lon=3))
+
+    def test_is_east_of_boundary_point_crosses_180(self):
+        self.assertFalse(geohash.is_east_of(origin_point_lon=173, boundary_point_lon=-178, test_point_lon=179))
+
+    def test_is_east_of_boundary_point_crosses_0(self):
+        self.assertFalse(geohash.is_east_of(origin_point_lon=-7, boundary_point_lon=2, test_point_lon=-1))
 
